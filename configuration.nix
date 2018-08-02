2b28a6b3e80bcdb387350a2fa92ecd839f529125
jjw <jacobjwebber@gmail.com>
2018-08-02 17:32:06 +0100

    Adding nixos config

---
+++ configuration.nix
@@ -0,0 +1,144 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;
+
+  # Select internationalisation properties.
+  # i18n = {
+  #   consoleFont = "Lat2-Terminus16";
+  #   consoleKeyMap = "us";
+  #   defaultLocale = "en_US.UTF-8";
+  # };
+
+  # Set your time zone.
+  time.timeZone = "Europe/London";
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    wget vim
+    wget firefox
+    wget chromium
+    
+    #haskellPackages.xmonadContrib
+    #haskellPackages.xmonadExtras
+    ghc
+    ocaml
+    opam
+    gnum4
+    unzip
+    gcc
+    clang
+    binutils-unwrapped
+
+    dmenu
+    xscreensaver
+    xclip
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.bash.enableCompletion = true;
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable the X11 windowing system.
+  #services.xserver.enable = true;
+  #services.xserver.layout = "gb";
+  #services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  #services.xserver.libinput.enable = true;
+
+  services = {
+    # Enable the OpenSSH daemon.
+    openssh.enable = true;
+
+    gnome3 = {
+      tracker.enable = false; # I don't use tracker
+      gnome-keyring.enable = true;
+    };
+    
+    # Enable the X11 windowing system.
+    xserver = {
+      enable = true;
+      layout = "gb";
+      libinput.enable = true;
+
+      # Gnome desktop
+      # * Slightly more familiar than KDE for people who are used to working with Ubuntu
+      # * Gnome3 works out of the box with xmonad
+      desktopManager = {
+        gnome3.enable = true;
+        default = "gnome3";
+      };
+  
+      # Enable XMonad Desktop Environment. (Optional)
+      windowManager = {
+        xmonad.enable = true; 
+        xmonad.enableContribAndExtras = true;
+      };
+    };
+  };
+
+  # Enable the KDE Desktop Environment.
+  #services.xserver.displayManager.sddm.enable = true;
+  #services.xserver.desktopManager.plasma5.enable = true;
+  # Allow nonfree
+  nixpkgs.config.allowUnfree = true;
+
+  # Audio
+  hardware.pulseaudio.enable = true;
+  sound.mediaKeys = {
+    enable = true;
+    volumeStep = "5%";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.extraUsers.jjw = {
+    name = "jjw";
+    extraGroups = [
+      "wheel" "disk" "networkmanager" "systemd-journal"
+    ];
+    createHome = true;
+    home = "/home/jjw";
+    isNormalUser = true;
+    uid = 1000;
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "18.03"; # Did you read the comment?
+
+}
